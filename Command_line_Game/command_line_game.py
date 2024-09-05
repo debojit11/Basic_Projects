06f04f7fbb1ba013eb260bb295a2d74476c06934
debojit11 <debojitchoudhury117@gmail.com>
2024-09-05 20:17:22 +0530

    second commit

--- Command_line_Game/command_line_game.py
+++ Command_line_Game/command_line_game.py
@@ -9,44 +9,44 @@
 #If they have the sword from the other room, then they will be able to defeat it and win the game.
 #If they don't have the sword, then they will be eaten by the dragon and lose the game.
 
-name = input("Enter Your Name: ")
+name = input(f"{'Enter Your Name: ':^30}")
 print("Welcome, " + name + ", to the land of adventure!")
 print("You are standing in front of two doors. One is to the left and the other is to the right.")
-choice = input("Which door do you want to choose? (left/right): ")
+choice = input(f'{"Which door do you want to choose? (left/right): ":^30}')
 
 has_sword = False  # Flag to track whether the player has taken the sword
 
 if choice == "left":
-    print("You are in a room with no doors. It is empty.")
-    choice2 = input("Do you want to stay here? (yes/no): ")
+    print(f'{"You are in a room with no doors. It is empty.":^30}')
+    choice2 = input(f'{"Do you want to stay here? (yes/no): ":^30}')
     if choice2 == "yes":
-        print("You are still in the empty room.")
+        print(f'{"You are still in the empty room.":^30}')
     elif choice2 == "no":
-        print("You are back in front of the two doors.")
+        print(f'{"You are back in front of the two doors.":^30}')
     else:
-        print("Invalid choice. Please choose yes or no: ")
+        print(f'{"Invalid choice. Please choose yes or no: ":^30}')
     
-    choice3 = input("Do you want to look around? (yes/no): ")
+    choice3 = input(f'{"Do you want to look around? (yes/no): ":^30}')
     if choice3 == "yes":
-        print("You see a sword on the ground.")
-        choice4 = input("Do you want to take the sword? (yes/no): ")
+        print(f'{"You see a sword on the ground.":^30}')
+        choice4 = input(f'{"Do you want to take the sword? (yes/no): ":^30}')
         if choice4 == "yes":
             has_sword = True
-            print("You took the sword!")
+            print(f'{"You took the sword!":^30}')
         else:
-            print("You left the sword.")
-    print("You return to the two doors.")
+            print(f'{"You left the sword.":^30}')
+    print(f'{"You return to the two doors.":^30}')
 while choice != "right":
-    choice= input("Now, you must choose the right door to proceed. (right): ")
+    choice= input(f'{"Now, you must choose the right door to proceed. (right): ":^30}')
 if choice == "right":
-    print("You are in a room with a dragon!")
-    choice5 = input("Do you want to fight the dragon? (yes/no): ")
+    print(f'{"You are in a room with a dragon!":^30}')
+    choice5 = input(f'{"Do you want to fight the dragon? (yes/no): ":^30}')
     if choice5 == "yes":
         if has_sword:
-            print("You defeated the dragon and won the game!")
+            print(f'{"You defeated the dragon and won the game!":^30}')
         else:
-            print("You were eaten by the dragon and lost the game!")
+            print(f'{"You were eaten by the dragon and lost the game!":^30}')
     else:
-        print("You chose not to fight the dragon and left the room.")
+        print(f'{"You chose not to fight the dragon and left the room.":^30}')
 
-print("Thank you for playing!")
+print(f'{"Thank you for playing!":^30}')
